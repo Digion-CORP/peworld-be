5a7369a7802a972a59f9bc850f718d8fd44a3faf
Ahmad Wahyudi <108715816+ahmadvvahyudi@users.noreply.github.com>
2022-08-08 21:46:26 +0700

    feat: add experience model, controller, routes

---
+++ controller/experienceController.js
@@ -0,0 +1,63 @@
+const {
+	getAllExperiences,
+	getExperienceById,
+	addExperience,
+	updateExperience,
+	removeExperience,
+} = require("../model/experienceModels")
+
+const getAllExperiences = async (req, res) => {
+	try {
+		const results = await getAllExperiences()
+		res.status(200).json(results)
+	} catch (error) {
+		res.status(400).json(error)
+	}
+}
+
+const getExperienceById = async (req, res) => {
+	try {
+		const results = await getExperienceById(req, res)
+		res.status(200).json(results)
+	} catch (error) {
+		res.status(400).json(error)
+	}
+}
+
+const addExperience = async (req, res) => {
+    try{
+        const results = await addExperience(req, res)
+        res.status(200).json(results)
+    }
+    catch(error){
+        res.status(400).json(error)
+    }
+}
+
+const updateExperience = async (req, res) => {
+    try{
+        const results = await updateExperience(req, res)
+        res.status(200).json(results)
+    }
+    catch(error){
+        res.status(400).json(error)
+    }
+}
+
+const removeExperience = async (req, res) => {
+    try{
+        const results = await removeExperience(req, res)
+        res.status(200).json(results)
+    }
+    catch(error){
+        res.status(400).json(error)
+    }
+}
+
+module.exports = {
+    getAllExperiences,
+    getExperienceById,
+    addExperience,
+    updateExperience,
+    removeExperience,
+}
