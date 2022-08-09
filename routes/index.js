b5c4e24babfda2ab558582082afb236b3a4e66f5
Ahmad Wahyudi <108715816+ahmadvvahyudi@users.noreply.github.com>
2022-08-09 16:43:05 +0700

    Revert "feat add experience feature not fix"

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,11 @@ const authRouter = require('./authRouter');
 const profileRouter = require('./profileRouter');
 const skillRouter = require('./skillRouter');
 const portofolioRouter = require('./portofolioRouter');
-const experienceRouter = require('./experienceRouter');
 
 app.use('/auth', authRouter);
 app.use('/profile', profileRouter);
 app.use('/skill', skillRouter);
 app.use('/portofolio', portofolioRouter);
-app.use('/experience', experienceRouter);
 
 module.exports = app;
 
