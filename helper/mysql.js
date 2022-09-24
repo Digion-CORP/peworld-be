9113e899466f0d9f754a1900d876cad7dfa7d329
zainulmbrk <zainulmbrk.id@gmail.com>
2022-09-25 02:31:24 +0800

    feat: fixing dependencies

--- helper/mysql.js
+++ helper/mysql.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const mysql = require('mysql')
+const mysql = require('mysql2')
 const { host, user, password, database } = process.env
 
 const db = mysql.createConnection({
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const path = require('path')
 
 const app = express()
-const { port } = process.env
+const { port } = process.env.PORT || 5000
 const db = require('./helper/mysql');
 const router = require('./routes/index')
 
